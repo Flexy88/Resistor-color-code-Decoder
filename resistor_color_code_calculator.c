aa5472f5e59a2f564ffd179c92ab6b97116cd0a6
Flexy88 <130561023+Flexy88@users.noreply.github.com>
2023-04-12 14:36:57 +0300

    removed some unwanted texts

--- resistor_color_code_calculator.c
+++ resistor_color_code_calculator.c
@@ -1,15 +1,4 @@
 
-/****                                      
-                                                    **-- DESCRIPTION --**
-
-      Resistors are basic components in any given circuits. Resistors are manufactured in small sizes rendering it impossible to 
-	  label them with their values. Due to this limitation, their value is indicated in form of color bands which are decoded to 
-	  get their value. Engineers have to depend on color-charts or mmemonics, pen and paper to find this value. There may be 
-	  instances of inaccuracy which can prove fatal to sensitive systems. The below program serves to calculate the resistance of 
-	  the common 4-band resistor.By just inputting the colors of the resistor, the user can get the value of the resistor.
-	  The time spent and accuracy gained from this program is of prime significance to the user.
-                                                                                                                            	  
-																																  ****/
 
 #include <stdio.h>
 #include <math.h>
